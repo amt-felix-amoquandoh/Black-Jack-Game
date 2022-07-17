508b5a62aea1c876c30e282559a0b12ffe533415
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-17 23:50:24 +0000

    Update sript.js

--- js/sript.js
+++ js/sript.js
@@ -4,11 +4,14 @@ let sum = firstCard + secondCard;
 
 let hasBlackjack = false;
 let isGameOn = true;
+let gameAlert = ""
 
 if (sum <= 20){
-
+    gameAlert = "Try again?"
 } else if (sum === 21){
    hasBlackjack = true;
+   gameAlert = "wohoo you got blackjack!!!!"
 } else {
    isGameOn = false
+   gameAlert = "Game Over!"
 };
