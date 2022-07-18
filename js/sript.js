db75e93d2a4029e6a1b4afb461c1ebab3bc7757c
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-18 18:11:23 +0000

    game files update

--- js/sript.js
+++ js/sript.js
@@ -1,5 +1,6 @@
-let firstCard = 12;
-let secondCard = 9;
+let firstCard = 9;
+let secondCard = 8;
+let allCards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 
 let hasBlackjack = false;
@@ -8,9 +9,14 @@ let gameMessage = ""
 
 let message = document.getElementById("gameAlert");
 let sumMessage = document.getElementById("sumOfCards");
-
+let cards = document.getElementById("numOfCards");
 
 function startGame (){
+  renderGame();
+}
+
+function renderGame (){
+  cards.textContent = "Cards: " + allCards[0] + " " + allCards[1];
   sumMessage.textContent = "Sum: " + sum;
   if (sum <= 20){
     gameMessage = "Try again? 🤔"
@@ -23,4 +29,13 @@ function startGame (){
 };
   gameAlert.textContent = gameMessage;
 }
+
+
+function newGame(){
+  let newCard = 4;
+  sum += newCard;
+  
+  allCards.push(newCard);
+  renderGame();
+};
 
