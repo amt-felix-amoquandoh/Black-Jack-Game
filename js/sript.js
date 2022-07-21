d51166a1532e77961806bc0882544b0c9a3002ab
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-21 15:31:14 +0000

    game files updated

--- js/sript.js
+++ js/sript.js
@@ -1,22 +1,50 @@
-let firstCard = 9;
-let secondCard = 8;
-let allCards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
-
-let hasBlackjack = false;
+let allCards = []
+let sum = 0;
 let isGameOn = true;
-let gameMessage = ""
+let hasBlackjack = false;
+let gameMessage = " ";
+let player = {
+  Name : "Asher",
+  Balance : 180
+}
 
+
+
+let playerScore = document.getElementById("playerScore");
 let message = document.getElementById("gameAlert");
 let sumMessage = document.getElementById("sumOfCards");
 let cards = document.getElementById("numOfCards");
 
+playerScore.textContent = player.Name + ": Ghc" + player.Balance;
+
+
+function getRandomCard(){
+ let randomCard = Math.floor(Math.random() * 13) + 1;
+ if (randomCard === 1){ 
+  return 11;
+ } else if (randomCard > 10){
+  return 10;
+ } else {
+  return randomCard;
+ }  
+}
+
+
+
 function startGame (){
+  let isGameOn = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  let allCards = [firstCard, secondCard]
+  let sum = firstCard + secondCard;
   renderGame();
 }
 
 function renderGame (){
-  cards.textContent = "Cards: " + allCards[0] + " " + allCards[1];
+  cards.textContent = "Cards: " ;
+  for(let i = 0; i < allCards.length; i++){
+    cards.textContent += allCards[i] + " ";
+  }
   sumMessage.textContent = "Sum: " + sum;
   if (sum <= 20){
     gameMessage = "Try again? 🤔"
@@ -32,10 +60,18 @@ function renderGame (){
 
 
 function newGame(){
-  let newCard = 4;
-  sum += newCard;
+  if (isGameOn === true && hasBlackjack === false){
+    newCard = getRandomCard();
+    sum += newCard;
+    
+    allCards.push(newCard);
+    renderGame();
+  } else {
+    startGame();
+  }
   
-  allCards.push(newCard);
-  renderGame();
 };
+
+
+
 
